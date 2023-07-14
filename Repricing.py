18b57f29730296182c6a06474687202944eb563d
LeopardX99 <40331060+LeopardX99@users.noreply.github.com>
2023-07-14 18:25:38 +0530

    Update Repricing.py

--- Repricing.py
+++ Repricing.py
@@ -4,29 +4,29 @@ import requests
 
 st.write("Hello")
 
-# # Set the necessary parameters
-# host = "https://dbc-620f667e-7348.cloud.databricks.com"
-# token = "dapia3599098eeea5f35eed792cdb361a438"
-# path = "/mnt/s3a://itx-dbm-dbw-pricinganalytics-dev/FileStore/tables/IQVIA_mapping/0019_part_00.csv"
+# Set the necessary parameters
+host = "https://dbc-620f667e-7348.cloud.databricks.com"
+token = "dapia3599098eeea5f35eed792cdb361a438"
+path = "/mnt/s3a://itx-dbm-dbw-pricinganalytics-dev/FileStore/tables/IQVIA_mapping/0019_part_00.csv"
 
-# # Create the API endpoint URL
-# url = f"{host}/dbfs/read?path={path}"
+# Create the API endpoint URL
+url = f"{host}/dbfs/read?path={path}"
 
-# # Set the headers with the access token
-# headers = {"Authorization": f"Bearer {token}"}
+# Set the headers with the access token
+headers = {"Authorization": f"Bearer {token}"}
 
-# # Send the GET request
-# response = requests.get(url, headers=headers, verify=False)
+# Send the GET request
+response = requests.get(url, headers=headers, verify=False)
 
 
-# # Check if the request was successful
-# if response.status_code == 200:
-#     # Access the content of the response
-#     content = response.json()
-#     # Process the content as needed
-#     st.write(content)
-# else:
-#     st.write(f"Request failed with status code {response.status_code}: {response.text}")
+# Check if the request was successful
+if response.status_code == 200:
+    # Access the content of the response
+    content = response.json()
+    # Process the content as needed
+    st.write(content)
+else:
+    st.write(f"Request failed with status code {response.status_code}: {response.text}")
 
 
 
